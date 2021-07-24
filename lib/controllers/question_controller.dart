93a79d7e70b721a833cf8f85fdcc43348ad51a6a
Jiho Park <jiho5993@naver.com>
2021-07-24 17:27:31 +0900

    question card

--- lib/controllers/question_controller.dart
+++ lib/controllers/question_controller.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:quiz_app/models/Questions.dart';
 
 class QuestionController extends GetxController with SingleGetTickerProviderMixin {
   // Lets animated our progress bar
@@ -8,6 +11,38 @@ class QuestionController extends GetxController with SingleGetTickerProviderMixi
   Animation _animation;
   // so that we can access our animation outside
   Animation get animation => this._animation;
+
+  PageController _pageController;
+  PageController get pageController => this._pageController;
+
+  List<Question> _questions = sample_data
+      .map(
+        (question) => Question(
+          id: question['id'],
+          question: question['question'],
+          options: question['options'],
+          answer: question['answer_index']
+        )
+      )
+      .toList();
+
+  List<Question> get questions => this._questions;
+
+  bool _isAnswered = false;
+  bool get isAnswered => this._isAnswered;
+
+  int _correctAns;
+  int get correcAns => this._correctAns;
+
+  int _selectedAns;
+  int get selectedAns => this._selectedAns;
+
+  // for more about obs please check documentation
+  RxInt _questionNumber = 1.obs;
+  RxInt get questionNumber => this._questionNumber;
+
+  int _numOfCorrectAns = 0;
+  int get numOfCorrectAns => this._numOfCorrectAns;
 
   @override
   void onInit() {
@@ -19,8 +54,56 @@ class QuestionController extends GetxController with SingleGetTickerProviderMixi
       });
 
     // start our animation
-    _animationController.forward();
-
+    // Once 60s is completed go to the next question
+    _animationController.forward().whenComplete(nextQuestion);
+    _pageController = PageController();
     super.onInit();
+  }
+
+  // called just before the Controller is deleted from memory
+  @override
+  void onClose() {
+    super.onClose();
+    _animationController.dispose();
+    _pageController.dispose();
+  }
+
+  void checkAns(Question question, int selectedIndex) {
+    // because once user press any option then it will run
+    _isAnswered = true;
+    _correctAns = question.answer;
+    _selectedAns = selectedIndex;
+
+    if(_correctAns == _selectedAns) _numOfCorrectAns++;
+
+    // it will stop the counter
+    _animationController.stop();
+    update();
+
+    // Once user select an answer after 3s it will go to the next question
+    Future.delayed(Duration(seconds: 3) , () {
+      nextQuestion();
+    });
+  }
+
+  void nextQuestion() {
+    if(_questionNumber.value != _questions.length) {
+      _isAnswered = false;
+      _pageController.nextPage(
+        duration: Duration(milliseconds: 250), 
+        curve: Curves.ease
+      );
+
+      // Reset the counter
+      _animationController.reset();
+
+      // Then start it again
+      // Once timer is finish go to the next question
+      _animationController.forward().whenComplete(nextQuestion);
+    }
+  }
+
+  void updateTheQnNum(int index) {
+    _questionNumber.value = index + 1;
   }
 }
--- lib/screens/quiz/quiz_screen.dart
+++ lib/screens/quiz/quiz_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
 
 import 'components/body.dart';
 
 class QuizScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
-          TextButton(onPressed: () {}, child: Text("Skip"))
+          TextButton(onPressed: _controller.nextQuestion, child: Text("Skip"))
         ],
       ),
       body: Body(),
